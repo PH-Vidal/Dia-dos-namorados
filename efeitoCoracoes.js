1a68407c3c398bf2911d210ea7b87317a1ba45bf
imgladiador <vidal.pedro@pucpr.edu.br>
2025-06-12 10:44:51 -0300

    Atualiza página com novas mudanças

---
+++ efeitoCoracoes.js
@@ -0,0 +1,14 @@
+function criarCoracao() {
+  const coracao = document.createElement('div');
+  coracao.classList.add('coracao');
+  coracao.textContent = '❤️';
+  coracao.style.left = Math.random() * 100 + 'vw';
+  coracao.style.fontSize = (Math.random() * 20 + 10) + 'px';
+  document.querySelector('.coracoes').appendChild(coracao);
+
+  setTimeout(() => {
+    coracao.remove();
+  }, 5000);
+}
+
+setInterval(criarCoracao, 300);
