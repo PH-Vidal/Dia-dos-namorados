c5a2669b5ad6badf684d5f8b532d3d2932fd9a7b
imgladiador <vidal.pedro@pucpr.edu.br>
2025-06-12 10:56:53 -0300

    Aumento nos corações

--- efeitoCoracoes.js
+++ efeitoCoracoes.js
@@ -3,7 +3,7 @@ function criarCoracao() {
   coracao.classList.add('coracao');
   coracao.textContent = '❤️';
   coracao.style.left = Math.random() * 100 + 'vw';
-  coracao.style.fontSize = (Math.random() * 20 + 10) + 'px';
+  coracao.style.fontSize = (Math.random() * 20 + 20) + 'px';
   document.querySelector('.coracoes').appendChild(coracao);
 
   setTimeout(() => {
